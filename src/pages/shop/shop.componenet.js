06762c7848845136ef4d797e1487859603a9491a
hz7451 <58805643+hz7451@users.noreply.github.com>
2020-12-15 15:59:52 +0800

    create shop data and array into reducer created respective selectors and updated corresponding compnetn

--- src/pages/shop/shop.componenet.js
+++ src/pages/shop/shop.componenet.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/preview-collection/preview-collection';
+import { Route } from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/collections-overview'
+import CollectionPage from '../Collection/collection';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            collections:SHOP_DATA    
-        };
-
-    }
-
-    render () {
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'> 
-                {
-                    collections.map(({ id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-
-            </div>)
-    }
-}
-
-export default ShopPage
+const ShopPage = ({match}) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+    </div>
+)
+export default ShopPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,22 @@ import { createStore, applyMiddleware } from 'redux';
 
 import logger from 'redux-logger';
 
+import {persistStore} from 'redux-persist';
+
 import rootReducer from  './root-reducer';
 
 const middleware = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware))
+export const store = createStore(rootReducer, applyMiddleware(...middleware))
 
-export default store;
+export const persistor = persistStore(store);
+
+const storepersist = {
+    store,
+    persistor
+};
+
+export default storepersist;
+
+
+// export default { store, persistStore }; In case for backup
